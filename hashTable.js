07e62fe9fbc3b2b4bd790b8b937e02500fbecbb5
Hailey Petway <petwayh@gmail.com>
2014-07-28 16:15:10 -0700

    fixing indent, spacing and adding comment

--- hashTable.js
+++ hashTable.js
@@ -1,6 +1,7 @@
 //This is an example of a HashTable with a simple Hash Function and a Linked List for collisions.
+//Values are distributed more uniformly in a hash table if the size of the table is a prime number.
 
-var LinkedList = function () {
+var LinkedList = function() {
   this.head = null;
   this.tail = null;
   this.add = function(value) {
@@ -11,29 +12,30 @@ var LinkedList = function () {
   };
 };
 
-var Node = function (value) {
+var Node = function(value) {
   this.value = value;
   this.next = null;
 };
 
 var HashTable = function(max) {
-    this.max = max;
-    this.storage = [];
-    for (var i = 0; i < max; ++i) {
-        this.storage[i] = new LinkedList();
+  this.max = max;
+  this.storage = [];
+  for (var i = 0; i < max; ++i) {
+    this.storage[i] = new LinkedList();
+  }
+  this.hash = function(value) {
+    var sum = 0;
+    for (var i = 0; i < value.length; ++i) {
+      sum += value[i].charCodeAt() - 97;
     }
-    this.hash = function (value) {
-        var sum = 0;
-        for (var i = 0; i < value.length; ++i) {
-            sum += value[i].charCodeAt() - 97;
-        }
-        return sum % this.max;
-    };
-    this.add = function (value) {
-        var index = this.hash(value);
-        this.storage[index].add(value);
-    };
+    return sum % this.max;
+  };
+  this.add = function(value) {
+    var index = this.hash(value);
+    this.storage[index].add(value);
+  };
 };
+
 var hash = new HashTable(5);
 hash.add('i');
 hash.add('would');
