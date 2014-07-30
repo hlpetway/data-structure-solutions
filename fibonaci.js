fc4108f13bd151c4b6bcd3b5c6a48693c6d2de51
Hailey Petway <petwayh@gmail.com>
2014-07-30 09:18:07 -0700

    adding the fib sequence solution

---
+++ fibonaci.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//generating the fibonaci sequence in JS
+var fibonaci = function (length) {
+  var fib = [];
+  fib[0] = 0;
+  fib[1] = 1;
+
+  for(var i = 2; i <= length; i++){
+    fib[i] = fib[i-2] + fib[i-1];
+  }
+  return fib;
+};
+
+console.log(fibonaci(10));
+
+
+//returning and storing the sequence in the function as a variable.
+var fibonaci = (function (length) {
+  var fib = [];
+  fib[0] = 0;
+  fib[1] = 1;
+
+  for(var i = 2; i <= 10; i++){
+    fib[i] = fib[i-2] + fib[i-1];
+  }
+  return fib;
+})();
+
+console.log(fibonaci);
