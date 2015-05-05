870c598d0141fe6792c98d0c9075f4b176c46af6
hlpetway <hlpetway@gmail.com>
2015-05-05 08:48:16 -0700

    adding the recursive soltuion to mergeSortedArray problem

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,7 +1,9 @@
+//
 // Given two sorted integer arrays A and B, merge B into A as one sorted array.
 // You may assume that A has enough space to hold additional elements from B.
 // The number of elements initialized in A and B are m and n respectively.
-
+// ----------------------------------------------------------------------------------
+//
 // ** Iterative Solution ***
 
 var a = [1,2,3];
@@ -41,4 +43,39 @@ var mergeArrays = function (arrayA,arrayB) {
 var c = [1,3,4];
 var d = [2,5,6];
 
-mergeArrays(c, d);
+//mergeArrays(c, d);
+
+
+// ----------------------------------------------------------------------------------
+//
+// ** Recursive Solution **
+//
+
+var recursiveMergeArrays = function (arrayA,arrayB,counter) {
+  var aCurrentPosition = counter;
+  var bCurrentPosition = counter;
+  var length = arrayA.length * arrayB.length;
+
+  if((arrayA.length === 0) || (arrayB.length === 0)) {
+    console.log("Error: One or More Arrays are Empty");
+    return;
+  }
+  if((arrayB[bCurrentPosition] > arrayA[aCurrentPosition]) && (arrayB[bCurrentPosition] < arrayA[aCurrentPosition+1])) {
+      arrayA.splice(aCurrentPosition+1, 0, arrayB.shift());
+      console.log("Inserting into Array A: " + arrayA);
+  }
+  else if((arrayB[bCurrentPosition] > arrayA[aCurrentPosition]) && (arrayB[bCurrentPosition] > arrayA[aCurrentPosition+1])) {
+      aCurrentPosition++;
+  }
+  else if((arrayB[bCurrentPosition] > arrayA[aCurrentPosition]) && (arrayA[aCurrentPosition+1] === undefined)) {
+      arrayA.push(arrayB.shift());
+      console.log("Pushing to end of Array A: " + arrayA);
+  }
+  recursiveMergeArrays(arrayA, arrayB, counter+1);
+
+  console.log("Array length: " + arrayA.length +": " + arrayA);
+  console.log("Array length: " + arrayB.length +": " + arrayB);
+  return arrayA;
+};
+
+mergeArrays(a,b,0);
