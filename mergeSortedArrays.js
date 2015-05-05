1ed57fc362258d73f422cb99e26b3e76c751aca2
hlpetway <hlpetway@gmail.com>
2015-05-05 08:30:50 -0700

    complete iterative soltuion

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -2,13 +2,15 @@
 // You may assume that A has enough space to hold additional elements from B.
 // The number of elements initialized in A and B are m and n respectively.
 
+// ** Iterative Solution ***
+
 var a = [1,2,3];
 var b = [4,5,6];
 
 var mergeArrays = function (arrayA,arrayB) {
   var aCurrentPosition = 0;
   var bCurrentPosition = 0;
-  var length = arrayA.length + arrayB.length;
+  var length = arrayA.length * arrayB.length;
 
   if((arrayA.length === 0) || (arrayB.length === 0)) {
     console.log("Error: One or More Arrays are Empty");
@@ -16,23 +18,27 @@ var mergeArrays = function (arrayA,arrayB) {
   }
   for(var i = 0; i < length; i++) {
       if((arrayB[bCurrentPosition] > arrayA[aCurrentPosition]) && (arrayB[bCurrentPosition] < arrayA[aCurrentPosition+1])) {
-        arrayA.push(arrayB.shift());
-        bCurrentPosition++;
+        arrayA.splice(aCurrentPosition+1, 0, arrayB.shift());
+        console.log("Inserting into Array A: " + arrayA);
       }
       else if((arrayB[bCurrentPosition] > arrayA[aCurrentPosition]) && (arrayB[bCurrentPosition] > arrayA[aCurrentPosition+1])) {
         aCurrentPosition++;
-        console.log("Incrementing Array A: " + arrayA);
       }
       else if((arrayB[bCurrentPosition] > arrayA[aCurrentPosition]) && (arrayA[aCurrentPosition+1] === undefined)) {
-        arrayA.push(arrayB.shift()); //cannot push on a value that is undefined.. take a look into this issue.
-        bCurrentPosition++;
+        arrayA.push(arrayB.shift());
+        console.log("Pushing to end of Array A: " + arrayA);
       }
   }
   console.log("Array length: " + arrayA.length +": " + arrayA);
   console.log("Array length: " + arrayB.length +": " + arrayB);
+  return arrayA;
 };
 
-mergeArrays(a,b);
+//mergeArrays(a,b);
 
-//We're left with the last value in Array B. I think this is due to the incrementing of the position.
-//Troubleshoot this!
+//This also works if the arrays are sorted but are not complete with consecutive integers.
+
+var c = [1,3,4];
+var d = [2,5,6];
+
+mergeArrays(c, d);
