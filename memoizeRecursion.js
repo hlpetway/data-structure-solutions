b5396a8f84d671bb658770b88c6934031fe2808a
hlpetway <hlpetway@gmail.com>
2015-05-12 08:36:29 -0700

    fixing examples

--- memoizeRecursion.js
+++ memoizeRecursion.js
@@ -1,58 +1,55 @@
 // Solving Fibonaci Recursively using Memoize
 // I'm going to compare the recursive and recursive with memoize solutions and their performance.
 // I will later compare Recursive-Memoize to Iteration
+// http://www.sitepoint.com/implementing-memoization-in-javascript/
 // ----------------------------------------------------------
 // ** Recursive with Memoize
 // Returns just the single value n
 
 var fibonacci = (function (  ) {
     var memo = [0, 1];
-    var fib = function (n) {
+    var fibMemo = function (n) {
         var result = memo[n];
         if (typeof result !== 'number') {
-            result = fib(n-1) + fib(n-2);
+            result = fibMemo(n-1) + fibMemo(n-2);
             memo[n] = result;
         }
         return result;
     };
-    return fib;
+    return fibMemo;
 }( ));
 
-console.log(fibonacci(5));
+//console.log(fibonacci(10));
 
 // --------------------------------------------------
 // ** Recursive Without Memoize **
-// TODO: Why does this only print the first two values?
+// Returns just the single value n
 
-var fibonaciSimple = function (length,counter) {
-  var fib = [];
-  fib[0] = 0;
-  fib[1] = 1;
-  if (counter < length){
-  fib[counter] = fib[counter-2] + fib[counter-1];
-  counter++;
-  return fibonaciSimple(length,counter);
-  }
-  else
-  return fib;
-};
-
-console.log(fibonaciSimple(6,2));
+function fibSimple(x) {
+    if (x === 0) {
+        return 0;
+    } else if (x === 1) {
+        return 1;
+    } else {
+        return fibSimple(x-1)+fibSimple(x-2);
+    }
+}
+console.log(fibSimple(10));
 
 // --------------------------------------------------
-// ** Iterative Solution **
+// ** Iterative Solution with Memoize **
 // using an immediately invoked function to store the sequence in variable
 //
 
 var fibonaciIterative = (function () {
-  var fib = [];
-  fib[0] = 0;
-  fib[1] = 1;
+  var fibIt = [];
+  fibIt[0] = 0;
+  fibIt[1] = 1;
 
   for(var i = 2; i <= 10; i++){
-    fib[i] = fib[i-2] + fib[i-1];
+    fibIt[i] = fibIt[i-2] + fibIt[i-1];
   }
-  return fib;
+  return fibIt;
 })();
 
-console.log(fibonaciIterative);
+//console.log(fibonaciIterative);
