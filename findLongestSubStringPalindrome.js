84f0ebaa72d9a6342478632da20b942b4ab150e0
hlpetway <hlpetway@gmail.com>
2015-05-13 19:33:48 -0700

    testing for both odd and even strings.

--- findLongestSubStringPalindrome.js
+++ findLongestSubStringPalindrome.js
@@ -1,12 +1,21 @@
 //Finding the longest palindromic substring is a classic problem of coding interview.
 
 // ** Finding Longest SubString Palindrome of a given String
+// This solution handles odd and even strings gracefully.
 // RESOURCE: http://stevekrenzel.com/articles/longest-palnidrome
 
 var exampleString = "dabcba";
 
 var longestPalindrome = function (string) {
+  if(string.isEmpty){
+    return null;
+  }
+  else if(string.length === 1){
+    return exampleString;
+  }
+
   var longest = string.substring(0,1);
+
   for(var i = 0; i< string.length; i++){
     var temp = helper(string, i, i);
     if(temp.length > longest.length){
@@ -28,4 +37,4 @@ var helper = function(string,begin,end){
   return string.substring(begin+1,end);
 };
 
-console.log(longestPalindrome(exampleString));
+longestPalindrome(exampleString);
