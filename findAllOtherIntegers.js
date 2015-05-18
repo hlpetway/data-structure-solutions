5d0716ef527e64ecabe68cbcc40f43c9defc015d
hlpetway <hlpetway@gmail.com>
2015-05-18 09:32:05 -0700

    troubleshoot this solution getting Not a Number for all but first position.

--- findAllOtherIntegers.js
+++ findAllOtherIntegers.js
@@ -17,6 +17,7 @@
 // -----------------------------------------------------------------------------
 //This solution is O(N^2)
 //
+/*
 var findAllOtherIntegers = function (array){
   var newArray = [];
   var multiply = "*";
@@ -36,3 +37,31 @@ var findAllOtherIntegers = function (array){
 var example = [1, 7, 3, 4];
 
 console.log(findAllOtherIntegers(example));
+*/
+
+//------------------------------------------------------------------------------
+//Using a Memoize-like approach
+//Loop through multiplying intergers before each index for each position in the array.
+//Then loop through backwards multiplying intergers after each index for each position. TADA!
+
+var get_product_of_all_ints_except_index = function(arrayOfInt){
+  productsOfAllIntegers = [1,1,1,1,1];
+  //There is no way in JS to instantiate an array
+  //of a specific size and with specific values other than looping through.
+  //This is to help solve the zero or an array of size 1 edge cases.
+  var product = 1;
+  for( var i = 0; i < arrayOfInt.length; i++){
+    productsOfAllIntegers[i] = product;
+    product *= arrayOfInt[i];
+  }
+  for(i = arrayOfInt.length; i > 0; i--){
+    productsOfAllIntegers[i] *= product;
+    product *= arrayOfInt[i];
+    console.log(productsOfAllIntegers[i]);
+  }
+  return productsOfAllIntegers;
+};
+
+var numbersToMultiply = [1,2,6,5,9];
+
+console.log(get_product_of_all_ints_except_index(numbersToMultiply));
