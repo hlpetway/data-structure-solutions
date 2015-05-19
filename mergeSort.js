90a188b48f12890d88e1a34ab9874b8893f1035f
hlpetway <hlpetway@gmail.com>
2015-05-19 13:37:05 -0700

    removing unnecesary comments

--- mergeSort.js
+++ mergeSort.js
@@ -5,9 +5,9 @@
 // ----------------------------------------------------------------------------
 
 var merge = function (left, right){
-    var result  = [],
-        il      = 0,
-        ir      = 0;
+    var result = [];
+    var il = 0;
+    var ir = 0;
 
     while (il < left.length && ir < right.length){
         if (left[il] < right[ir]){
@@ -16,26 +16,20 @@ var merge = function (left, right){
             result.push(right[ir++]);
         }
     }
-
     return result.concat(left.slice(il)).concat(right.slice(ir));
 };
 
 var mergeSort = function (items){
-
     // Terminal case: 0 or 1 item arrays don't need sorting
     if (items.length < 2) {
         return items;
     }
-
     var middle = Math.floor(items.length / 2),
         left    = items.slice(0, middle),
         right   = items.slice(middle);
-
     return merge(mergeSort(left), mergeSort(right));
 };
 
 var test = [2,6,1,5,4,3];
 
 console.log(mergeSort(test));
-
-//This works, but I need to understand how.
