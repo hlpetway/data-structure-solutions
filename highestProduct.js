8bde14a0b85a37bace651be6c04a70b41b536688
hlpetway <hlpetway@gmail.com>
2015-05-18 20:08:28 -0700

    starting next Interview Cake problem

---
+++ highestProduct.js
@@ -0,0 +1,21 @@
+// ** Interview Cake **
+//
+// Given an array_of_ints, find the highest_product you can get from three of the integers.
+// The input array_of_ints will always have at least three integers.
+// -----------------------------------------------------------------------------
+
+var test = [1,3,5,2,6,7];
+
+var findHighestProduct = function (arrayOfInts){
+  for( var i = 0; i < arrayOfInts.length; i++) {
+    var newArray = [];
+    //start our merge sort here, is this where we use our temp variable
+    //and push a value here and compare it to the rest of the array?
+  }
+
+};
+
+console.log(findHighestProduct(test));
+
+// Sort the array and then multiply the three greatest intergers?
+// We can do it in O(n log n) time, merge sort I believe.
