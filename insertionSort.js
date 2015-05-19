6ec629259fd0c0ca6417273ea6043c8ea8512973
hlpetway <hlpetway@gmail.com>
2015-05-18 18:27:14 -0700

    adding insertion sort example from Into to Algo textbook

---
+++ insertionSort.js
@@ -0,0 +1,19 @@
+// ** Insertion Sort **
+// Faster on a small data set than Merge, but is much slower as n grows very large.
+// Complexity of O(N^2)
+
+var insertionSort = function (arrayToSort){
+  for(var j = 1; j < arrayToSort.length; j++){
+    key = arrayToSort[j];
+    var i = j - 1;
+    while((i >= 0) && (arrayToSort[i] > key)){
+      arrayToSort[i+1] = arrayToSort[i];
+      i = i - 1;
+      arrayToSort[i+1] = key;
+    }
+  }
+  return arrayToSort;
+};
+
+var test = [5,2,4,6,1,3];
+console.log(insertionSort(test));
