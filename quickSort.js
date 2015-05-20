2cb51a88d0ffc3a3dabf2d3c37484816f30aa105
hlpetway <hlpetway@gmail.com>
2015-05-20 15:35:50 -0700

    adding notes to quick sort example

--- quickSort.js
+++ quickSort.js
@@ -25,34 +25,6 @@ var quickSorts = function (nums) {
   return quickSorts(left).concat(pivot, quickSorts(right));
 };
 
-console.log(quickSorts(test));
+console.log(quickSorts(test)); // returns [1,2,3,4,5,6]
 
-console.log(test.sort());
-
-Array.prototype.equals = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
-
-    // compare lengths - can save a lot of time
-    if (this.length != array.length)
-        return false;
-
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].equals(array[i]))
-                return false;
-        }
-        else if (this[i] != array[i]) {
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;
-        }
-    }
-    return true;
-};
-
-if(quickSorts(test).equals(test.sort())){
-  console.log("Both functions are equal");
-}
+console.log(test.sort());   // returns [1,2,3,4,5,6]
